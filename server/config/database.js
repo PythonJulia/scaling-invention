776a87302c615f8ad5dc4d9a495f7c1c57127a47
Python Julia DataScience (dotipynb.com) <71810412+PythonJulia@users.noreply.github.com>
2025-02-28 12:57:09 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,17 @@
+// server/config/database.js
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+sequelize.authenticate()
+    .then(() => console.log('Database connected successfully'))
+    .catch(err => console.error('Error connecting to the database:', err));
+
+module.exports = sequelize;
