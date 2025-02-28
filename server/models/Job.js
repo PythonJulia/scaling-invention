86bfce04c514990f7f841ae5d2ae5a904c26970d
Python Julia DataScience (dotipynb.com) <71810412+PythonJulia@users.noreply.github.com>
2025-02-28 12:56:33 +0530

    Update Job.js

--- server/models/Job.js
+++ server/models/Job.js
@@ -1,1 +1,24 @@
+// server/models/Job.js
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Job = sequelize.define('Job', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  company: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  salary: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = Job;
 
